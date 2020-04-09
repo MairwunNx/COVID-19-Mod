7465f31f9484cda2d3871e3a0040cc7b607a30e2
Pavel Erokhin (MairwunNx) <MairwunNx@gmail.com>
2020-04-09 23:45:20 +0300

    Version updated in build.gradle.kts.

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,7 +18,7 @@ plugins {
 }
 
 group = "me.mairwunnx.covid19"
-version = "1.0.0+MC-1.14.4"
+version = "1.0.1+MC-1.14.4"
 
 val Project.configureMinecraft
     get() = extensions.getByName<UserDevExtension>("minecraft")
